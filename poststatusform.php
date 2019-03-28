22bd45d3eb7d92e0103bfb4d84b9bd8a889aac5a
BobbieWare <robbie.ware@gmail.com>
2019-03-28 16:10:42 +1300

    Starting a1 webdev

---
+++ poststatusform.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Status Posting System</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1 style="font: bold">Status Posting System</h1>
+        <form name="postStatus" method="POST">
+            <input type="text" name="code" value="S0000" size="5" />
+            <input type="text" name="text" value="" />
+            <input type="radio" name="share" value="Public" />
+            <input type="radio" name="share" value="Friends" />
+            <input type="radio" name="share" value="Only Me" />
+            <input type="text" name="date" value="99/99/99" />
+            <input type="checkbox" name="permission" value="ON" />
+            
+        </form>
+    </body>
+</html>
+
